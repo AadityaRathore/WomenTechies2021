590fdfe025a86e3602213bda6003d3213df1c2db
AadityaRathore <77567230+AadityaRathore@users.noreply.github.com>
2021-03-14 21:02:02 +0530

    Add files via upload

---
+++ lib/data/Dishes.dart
@@ -0,0 +1,26 @@
+List<Map> dishes = [
+  {
+    "image": "assets/images/image012.jpg",
+    "title": "Roti Sabji",
+    "time": "25 min",
+    "calories": "426 kCal"
+  },
+  {
+    "image": "assets/images/image013.jpg",
+    "title": "Dosa",
+    "time": "10 min",
+    "calories": "200 kCal"
+  },
+  {
+    "image": "assets/images/image014.jpeg",
+    "title": "Dal Rice",
+    "time": "40 min",
+    "calories": "634 kCal"
+  },
+  {
+    "image": "assets/images/image015.jpg",
+    "title": "Burger",
+    "time": "60 min",
+    "calories": "1.238 kCal"
+  },
+];
